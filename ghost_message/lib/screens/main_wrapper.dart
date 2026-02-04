1d9a8946d1d63ef0a436a9168934a3989e684c92
Gun8742 <gun.b@ku.th>
2026-02-04 14:58:48 +0700

    custom navbar nidnoi

--- ghost_message/lib/screens/main_wrapper.dart
+++ ghost_message/lib/screens/main_wrapper.dart
@@ -3,12 +3,13 @@ import 'package:ghost_message/providers/navigation_provider.dart';
 import 'package:ghost_message/screens/home_screen.dart';
 import 'package:ghost_message/screens/profile_screen.dart';
 import 'package:ghost_message/screens/setting_screen.dart';
+import 'package:ghost_message/widgets/navbar_build.dart';
 import 'package:provider/provider.dart';
 
 class MainWrapper extends StatelessWidget {
   final List<Widget> _screens = [
-    HomeScreen(),
     ProfileScreen(),
+    HomeScreen(),
     SettingScreen()
   ];
 
@@ -16,9 +17,20 @@ class MainWrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     final navigationProvider = Provider.of<NavigationProvider>(context);
     return Scaffold(
-      body: IndexedStack(
-        index: navigationProvider.currentIndex,
-        children: _screens,
+      extendBody: true,
+      body: Stack(
+        children: [
+          IndexedStack(
+            index: navigationProvider.currentIndex,
+            children: _screens,
+          ),
+          Positioned(
+            bottom: 25,
+            left: 20,
+            right: 20,
+            child: BuildNavbar(context, navigationProvider)
+          )
+        ]
       )
     );
   }
---
+++ ghost_message/lib/widgets/navbar_build.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:ghost_message/providers/navigation_provider.dart';
+
+Widget BuildNavbar(BuildContext context, NavigationProvider navigationProvider) {
+  return ClipRRect(
+    borderRadius: BorderRadius.circular(15),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _navItem(context, 0, Icons.person_outline, "Profile", navigationProvider),
+        _navItemHome(context, 1, navigationProvider),
+        _navItem(context, 2, Icons.settings_outlined, "Settings", navigationProvider),
+      ],
+    )
+  );
+}
+
+Widget _navItem(BuildContext context, int index, IconData icon, String label, NavigationProvider navigationProvider) {
+  bool isSelected = navigationProvider.currentIndex == index;
+  return GestureDetector(
+    onTap: () {
+      navigationProvider.setIndex(index);
+    },
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: isSelected ? Colors.black : Colors.blueGrey),
+        Text(label, style: TextStyle(color: isSelected ? Colors.black : Colors.blueGrey, fontSize: 11)),
+      ],
+    )
+  );
+}
+
+Widget _navItemHome(BuildContext context, int index, NavigationProvider navProvider) {
+  bool isSelected = navProvider.currentIndex == index;
+  return GestureDetector(
+    onTap: () => navProvider.setIndex(index),
+    child: Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: isSelected ? Colors.black : Colors.white,
+      ),
+      child: const Icon(Icons.home, color: Colors.grey, size: 30),
+    ),
+  );
+}
